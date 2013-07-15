1c01de66e29debe11b896f00626e5b762f27ca95
Sean Zhang <psytown@qq.com>
2013-07-15 21:55:26 +0800

    time

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.ready = function() {
 
 
 function updateNumbers() {
-    var start_time = 1318694400;
+    var start_time = 1310694400;
     var duration = parseInt(Date.now() / 1000 - start_time);
 
     var seconds = duration % 60;
